309a84349cb251506dd879c5e73963308f0c0b45
magedsaif <magedsaif21@gmail.com>
2023-10-05 18:30:40 +0200

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
-# Script that sets up my web servers for the deployment of web_static
-apt-get -y update
-apt-get -y install nginx
-service nginx start
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo sh -c 'echo "Holberton school"> /data/web_static/releases/test/index.html'
+# sets up my web servers for the deployment of web_static
+
+sudo apt-get update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+
+sudo sh -c 'echo "Holberton school by maged" >/data/web_static/releases/test/index.html'
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '19i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
-service nginx restart
+sudo sh -c 'echo "\
+server {
+    listen 80;
+    add_header X_Served_by \$HOSTNAME;
+    listen [::]:80 default_server;
+    server_name magedsaif.tech;
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+}" > /etc/nginx/sites-available/default'
+
+sudo service nginx restart
