f12d7c9556d93008cc55f5af1ad8d1adf38d339f
magedsaif <magedsaif21@gmail.com>
2023-10-21 14:11:12 +0200

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Flask web application."""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """hello_world."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """hbnb."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """C is fun."""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Python is cool."""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run()
