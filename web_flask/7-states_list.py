667a0b168a0e01497efd53d541ee8ac63efde7e8
magedsaif <magedsaif21@gmail.com>
2023-10-22 10:21:20 +0200

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,6 +13,7 @@ def states_list():
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Close the storage."""
